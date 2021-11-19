5defdb2fa829f34a7172182ed4ce91b3e9ce66bc
yanazelieva <alunettt@gmail.com>
2021-11-20 00:54:58 +0200

    practice 11

---
+++ p11_zelyeva/p11.1_zelyeva.py
@@ -0,0 +1,24 @@
+def cons(head, tail=[]):
+    tail.insert(0, head)
+    return  tail
+
+# Перевірка
+l = cons(3,
+        cons(2,
+            cons(1, [])))
+print(f'Result: {l}')
+
+assert l == [3, 2, 1], 'Failed test 1'
+assert cons(1) == [1], 'Failed test 2'
+print('All tests good!')
+
+
+def sum(lst):
+    if not lst:
+        return 0
+    return lst[0] + sum(lst[1:])
+
+# Перевірка
+print(sum(l))
+assert sum(l) == 6, 'Failed on sum'
+print('All tests good!')
---
+++ p11_zelyeva/p11.2_zelyeva.py
@@ -0,0 +1,23 @@
+lst = []
+
+
+def rrange(begin, end, step=1):
+    if (begin > end and step > 0) or (begin < end and step < 0) or (step == 0):
+        return []
+    elif begin == end:
+        return lst
+    else:
+        return [begin, *rrange(begin + step, end, step)]
+
+
+# Перевірка
+
+x = rrange(1, 10)
+y = rrange(10, 1, -1)
+z = rrange(10, 1, 1)
+print(x, y, z)
+
+assert x == list(range(1, 10)), 'Failed test for simple range'
+assert y == list(range(10, 1, -1)), 'Failed test for reverse range'
+assert z == list(range(10, 1, 1)), 'Failed test for empty range'
+print('All tests good!')
