287808ed7c6de89094e7ce9c93a972442ff2877e
yanazelieva <91508710+yanazelieva@users.noreply.github.com>
2021-12-24 18:28:58 +0200

    Update p8.1_zelyeva.py

--- p8_zelyeva/p8.1_zelyeva.py
+++ p8_zelyeva/p8.1_zelyeva.py
@@ -1,14 +1,13 @@
-
 pose_estimation = "[BodyPart:0-(0.55, 0.17) score=0.81 BodyPart:1-(0.49, 0.27) score=0.85 BodyPart:2-(0.41, 0.26) score=0.67 BodyPart:3-(0.33, 0.37) score=0.72 BodyPart:4-(0.36, 0.48) score=0.78 BodyPart:5-(0.58, 0.27) score=0.81 BodyPart:6-(0.65, 0.38) score=0.88 BodyPart:7-(0.62, 0.48) score=0.86 BodyPart:8-(0.43, 0.48) score=0.60 BodyPart:9-(0.43, 0.66) score=0.67 BodyPart:10-(0.53, 0.79) score=0.56 BodyPart:11-(0.53, 0.48) score=0.56 BodyPart:12-(0.59, 0.66) score=0.75 BodyPart:13-(0.49, 0.80) score=0.50 BodyPart:14-(0.54, 0.15) score=0.73 BodyPart:15-(0.56, 0.15) score=0.85 BodyPart:16-(0.48, 0.16) score=0.81 BodyPart:17-(0.83, 0.18) score=0.79]"
-import re
 lsp = []
 lsd = []
 clear = "\d.\d{1,2}"
 fen = re.compile(clear)
 lsk = fen.findall(pose_estimation)
-d = len(lsk)
-for i in range(d):
-    lsd.append(float(lsk[1]))
-    lsd.append(float(lsk[2]))
-    lsp.append(float(lsk[3]))
-print("points: ", lsd,"\n scores: ",lsp)
+for i in range(len(lsk)):
+    if (i+1) % 3 == 0:
+        lsp.append(float(lsk[i]))
+    else:
+        lsd.append(float(lsk[i]))
+
+print("points: ", lsd, "\nscores: ", lsp)
